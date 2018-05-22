cf1eae58e88a091e9ec6700af3fade0e0f423ba3
KangSeyun <dcp.k953@gmail.com>
2018-05-22 12:56:34 +0900

    feat( ui) dashboard and lending page
    Signed-off-by: KangSeyun <dcp.k953@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }    from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LandingComponent } from './landing/landing.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    LandingComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
