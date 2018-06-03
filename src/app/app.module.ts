115849b011e3cedd19b0606a0936cd8015ad5160
KangSeyun <dcp.k953@gmail.com>
2018-06-03 15:54:07 +0900

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-import { FormsModule }    from '@angular/forms';
-
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { AppRoutingModule } from './app-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { LandingComponent } from './landing/landing.component'
-
-
 import { PagesModule } from './pages/pages.module';
 import { routing } from './app.routing';
+import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    DashboardComponent,
-    LandingComponent,
-  ],
   imports: [
     BrowserModule,
-    routing,
-    FormsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    PagesModule,
+    routing
   ],
-  
-  providers: [],
+  declarations: [
+    AppComponent,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
